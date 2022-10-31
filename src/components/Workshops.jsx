02814506e0d54546c024f26cff048f7998a62bf1
michaelxuw <michaelxuwrelated@gmail.com>
2022-10-31 17:16:35 +0100

    done

--- src/components/Workshops.jsx
+++ src/components/Workshops.jsx
@@ -1,18 +1,20 @@
 import {getWorkshops} from "../api";
-import {Link, Outlet} from "react-router-dom";
+import {Link, NavLink, Outlet} from "react-router-dom";
 
 export default function Workshops() {
     const workshops = getWorkshops();
+    const getClass = ({ isActive }) => (isActive ? "workshop-active" : null);
+
     return (
         <div className="container">
             <h1>Workshop Categories</h1>
             <ul className="workshops">
                 {workshops.map(work => (
-                    <Link to={work.id}>
+                    <NavLink to={work.id} className={getClass}>
                         <li key={work.id}>
                             {work.name}
                         </li>
-                    </Link>
+                    </NavLink>
                 ))}
             </ul>
             <Outlet/>
