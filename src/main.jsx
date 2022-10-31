7d9e2ba65d02b6a8081f3c55fe2c8d7c9bcd04b9
michaelxuw <michaelxuwrelated@gmail.com>
2022-10-31 15:58:40 +0100

    done

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/main.css'
-import Home from "./components/Home.jsx";
-import Workshops from "./components/Workshops";
+import Home from "./pages/Home.jsx";
+import Workshops from "./pages/Workshops.jsx";
 import Header from "./components/Header";
-import About from "./components/About.jsx";
+import About from "./pages/About.jsx";
+import Workshop from "./pages/Workshop";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
@@ -15,8 +16,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
                 <Routes>
                     <Route path="/" element={<Home titel="Welcome to SchoolHacks"/>}/>
-                    <Route path="workshops" element={<Workshops/>}/>
+                    <Route path="workshops" element={<Workshops/>}>
+                        <Route path="workshop" element={<Workshop/>}/>
+                    </Route>
                     <Route path="about" element={<About/>}/>
+                    <Route path="*" element={<main style={{pading: "1rem"}}><p>Nothing found</p></main>}/>
                 </Routes>
 
                 <footer className="container">
